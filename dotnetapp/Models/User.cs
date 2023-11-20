97156300cae8068e6d321c0c40e096e064d48acd
Iamneo <techadmin@iamneo.ai>
2023-11-20 11:38:52 +0000

    Backup Commit

--- dotnetapp/Models/User.cs
+++ dotnetapp/Models/User.cs
@@ -7,8 +7,8 @@ namespace dotnetapp.Models
     public class User
     {
         public int UserId{get;set;}
-        public string UserName{get;set;}
-        public string UserEmail{get;set;}
+        public string Name{get;set;}
+        public string Email{get;set;}
         public string password{get;set;}
     }
 
