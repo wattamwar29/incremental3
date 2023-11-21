1ee614276342f7001343b1e038ee37e64a961fb3
Iamneo <techadmin@iamneo.ai>
2023-11-21 06:19:48 +0000

    Backup Commit

--- angularapp/src/app/getplayers/getplayers.component.ts
+++ angularapp/src/app/getplayers/getplayers.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AdminService } from '../services/admin.service';
+ 
 @Component({
   selector: 'app-getplayers',
   templateUrl: './getplayers.component.html',
   styleUrls: ['./getplayers.component.css']
 })
 export class GetplayersComponent implements OnInit {
-
-  constructor() { }
-
+  playerdata: any[]=[]
+ 
+  constructor(private as:AdminService) {
+    this.as.GetAllPlayers().subscribe(data =>{this.playerdata.push(...data)})
+    console.log(this.playerdata)
+   }
+ 
   ngOnInit(): void {
   }
-
+ 
 }
