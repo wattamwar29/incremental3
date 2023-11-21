668e4512b3257e4dcaaf4e53ef01e69e8e3d1a41
Iamneo <techadmin@iamneo.ai>
2023-11-21 11:43:10 +0000

    Backup Commit

--- angularapp/src/app/editplayers/editplayers.component.ts
+++ angularapp/src/app/editplayers/editplayers.component.ts
@@ -1,3 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { AdminService } from '../services/admin.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Player } from '../models/player';
+@Component({
+  selector: 'app-editplayer',
+  templateUrl: './editplayer.component.html',
+  styleUrls: ['./editplayer.component.css']
+})
+export class EditplayerComponent implements OnInit {
+  constructor(private as:AdminService, private route: Router, private ar: ActivatedRoute) { }
+  playerdata: Player = {id:0,name:'',age:0,category:'',biddingprice:0, teamid:0}
+  id:number
+  ngOnInit(){
+    const tid = this.ar.snapshot.paramMap.get('id')
+    this.id = Number(tid)
+    this.getPlayer(this.id)
+  }
+  getPlayer(id:number){
+    this.as.getPlayer(id).subscribe((data:Player) => this.playerdata = data)
+  }
+  saveData(player:Player)
+  {
+    this.playerdata = player
+    this.as.editPlayers(this.playerdata).subscribe(() => {
+      alert("Record Edited")
+      this.route.navigate(['/GetPlayer'])
+    })
+  }
+}
+
+
+
 // import { Component, OnInit } from '@angular/core';
 // import { AdminService } from '../services/admin.service';
 // import { ActivatedRoute, Router } from '@angular/router';
