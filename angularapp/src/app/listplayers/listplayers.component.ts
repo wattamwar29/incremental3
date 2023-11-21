007d0e3f6cbb823a84181cc988cae440dfa4fa51
Iamneo <techadmin@iamneo.ai>
2023-11-21 04:49:36 +0000

    Backup Commit

--- angularapp/src/app/listplayers/listplayers.component.ts
+++ angularapp/src/app/listplayers/listplayers.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { AdminService } from '../services/admin.service';
- 
+import { IPlayer } from '../models/iplayer';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-listplayers',
   templateUrl: './listplayers.component.html',
   styleUrls: ['./listplayers.component.css']
 })
 export class ListplayersComponent implements OnInit {
- 
+
+
   playerdata: any[] = []
  
   constructor(private ps:AdminService) {
